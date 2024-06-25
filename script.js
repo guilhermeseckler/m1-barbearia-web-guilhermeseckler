fe6f0d76ba48b757eaef00117c26652bb8df12fa
Guilherme Seckler <guilhermeseckler@outlook.com>
2024-06-25 15:46:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,27 +19,62 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    for (let i = 0; i < barbearia.cortes.length; i++) {
+        if (barbearia.cortes[i].id === id) {
+            return barbearia.cortes[i]
+        }
+    }
+    return 'Corte não encontrado'
 }
 
 function buscaBarbaPorId(id) {
+    for (let i = 0; i < barbearia.barbas.length; i++){
+        if (barbearia.barbas[i].id === id) {
+            return barbearia.barbas[i]
+        }
+    }
+    return 'Barba não encontrada'
 }
 
 function verificaStatusBarbearia() {
+    if (barbearia.estaAberto) {
+        return 'Estamos abertos'
+    }
+    return 'Estamos fechados'
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas
 }
-
 function criaPedido(nomeCliente, corteId, barbaId) {
+    const corte = buscaCortePorId(corteId)
+    const barba = buscaBarbaPorId(barbaId)
+    const pedido = {
+        nome: nomeCliente,
+        pedidoCorte: corte.tipo,
+        pedidoCortePreco: corte.valor,
+        pedidoBarba: barba.tipo,
+        pedidoBarbaPreco: barba.valor
+    }
+    calculaTotal(pedido)
+    return pedido;
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i].id === id) {
+            lista[i].tipo = tipo
+            lista[i].valor = valor
+        }
+    }
+    return lista
 }
 
 function calculaTotal(pedido) {
+    let total = pedido.pedidoBarbaPreco + pedido.pedidoCortePreco
+    return total
 }
-  
-  
